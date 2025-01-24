bd0c2e980004ecb18e1e5a65c2cb7dd608520d3a
TFoster16 <northwesterntim@gmail.com>
2025-01-24 05:08:38 -0600

    program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,7 @@
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    print('Hello World')
 
 hello_world()
+#Timothy Foster, 1/24/25, Simple Hello World Program
